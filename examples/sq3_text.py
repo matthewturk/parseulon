759cddd0e4acb46455bbe883fb85782d7c1e4deb
Matthew Turk <matthewturk@gmail.com>
2015-06-04 20:20:39 -0500

    Parse image cells

--- examples/sq3_text.py
+++ examples/sq3_text.py
@@ -2,4 +2,5 @@ import parseulon
 
 m = parseulon.ResourceMapSCI0("SQ3/resource.map")
 print m.text[0].data.tostring()
-print m.view[0].view
+v = m.view[0].view
+print v
--- parseulon/view.py
+++ parseulon/view.py
@@ -13,6 +13,16 @@ class SCI0View(View):
         self.ngroups, self.bitmask, _ = struct.unpack("<HHI", sdata[:8])
         self.cell_indices = struct.unpack("<%sH" % self.ngroups,
             sdata[8:8+2*self.ngroups])
+        # These are composed of cells, each of which has image cells in it.
+        # So, let's split up the strings.
+        self.cells = []
+        # This next little bit is because sometimes start indices are
+        # duplicated, particularly when things are reversed.
+        end_indices = list(set(self.cell_indices + (len(sdata),)))
+        end_indices.sort()
+        for start_index in self.cell_indices:
+            ei = end_indices.index(start_index) + 1
+            self.cells.append(sdata[start_index:end_indices[ei]])
 
     def __repr__(self):
         return "%s - %s - %s" % (self.ngroups, self.bitmask, self.cell_indices)
